9839f8aa86340991b5cfa3cc2d440b3223125aba
Robert Pirritano <rpirritano@gmail.com>
2020-07-06 16:34:21 -0700

    added decrementQuantity and reomve

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -42,5 +42,13 @@ export class CartDetailsComponent implements OnInit {
     this.cartService.addToCart(theCartItem);
   }
 
+  decrementQuantity(theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem);
+  }
+
+  remove(theCartItem: CartItem) {
+    this.cartService.remove(theCartItem);
+  }
+
 
 }
